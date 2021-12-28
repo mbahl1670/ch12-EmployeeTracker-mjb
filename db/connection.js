1eea1df5db2b6035e2218c9656130ea5bc5f18f5
Michael <mbahl1670@gmail.com>
2021-12-27 22:30:23 -0600

    starting files

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+const password = require("./password");
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: password,
+  database: 'employeeDB'
+});
+
+module.exports = employeeDB;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const db = require("./db/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log("Database connected.");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});

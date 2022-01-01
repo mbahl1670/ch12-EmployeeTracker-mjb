074c84ea9a741c03b08e475a352279819bf2a1f1
Michael <mbahl1670@gmail.com>
2022-01-01 11:03:54 -0600

    managed to view department on employee table

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,5 @@
-SELECT emp.id, emp.first_name, emp.last_name, role.title AS job_title, role.salary AS salary, manager.first_name AS manager
+SELECT emp.id, emp.first_name, emp.last_name, role.title AS job_title, department.name AS department, role.salary AS salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
 FROM employee AS emp
 LEFT JOIN employee AS manager ON manager.id = emp.manager_id
-JOIN role ON emp.role_id = role.id;
--- JOIN department ON emp.role_id = role.department_id;
+JOIN role ON emp.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id;
--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function viewRoles() {
 };
 
 function viewEmployees() {
-    const sql = "SELECT emp.id, emp.first_name, emp.last_name, role.title AS job_title, role.salary AS salary, manager.first_name AS manager FROM employee AS emp LEFT JOIN employee AS manager ON manager.id = emp.manager_id JOIN role ON emp.role_id = role.id;"
+    const sql = `SELECT emp.id, emp.first_name, emp.last_name, role.title AS job_title, department.name AS department, role.salary AS salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee AS emp LEFT JOIN employee AS manager ON manager.id = emp.manager_id JOIN role ON emp.role_id = role.id LEFT JOIN department ON role.department_id = department.id;`
 
     db.query(sql, (err, res) => {
         if(err) {
